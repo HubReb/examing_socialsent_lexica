c626041045ff449ea2667aa0427b80f9cb7f56f7
hubert <hubert@cl.uni-heidelberg.de>
2017-07-31 20:32:04 +0200

    clean up clustering.py, keep old version for comparision later

--- clustering.py
+++ clustering.py
@@ -1,86 +1,178 @@
-#! /ust/bin/env python3
-# -*- coding: utf-8 -*_
-#
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+'''
+Extract all sentiments in each reddit and create a corresponding feature vector
+for each reddit. There are four types of feature vectores created:
+    normal: each feature has the sentiment value as specified in the subreddit
+    minimum: each feature has the sentiment value normal - standard derivation
+    maximum: each feture has the sentiment value normal + standard derivation
+    all: all three types of values are included in the feature vector making it
+        thrice as big
+For each view all feature vectors are clustered to detec similarities between the
+subreddits. For now, all clustering algorithms are applied.
+'''
 
 import os
 import numpy as np
 import csv
 import matplotlib
-matplotlib.use('pdf')
-import matplotlib.pyplot as plt
 import sklearn.cluster as cluster
-import seaborn as sns
 import spacy
 import hdbscan
+import json
+
 
 class SubredditData:
+    """
+    An object containing all raw and processed data in every given subreddit.
+
+    Attributes:
+        subreddits:
+            dictionary of form: subreddit:word:sentiment, standard derivation
+        sentiments:
+            dictionary of views (normal, min, max, all) containing feature vectors
+            of all subreddits
+        order:
+            list representing the order of the word in each feature vector
+    """
 
     def __init__(self, path):
+        """
+        Initialize an object containing all raw and processed data
 
-        subreddits = os.listdir(path)
-        self.subreddits_sentiments = {}
-        self.subreddits_sentiments_all = {}
-        self.subreddits_sentiments_minimum = {}
-        self.subreddits_sentiments_maximum = {}
+        Arguments:
+            path:   Path to the folder containing the subreddits
+        """
 
-        if "words.txt" in subreddits:
-            with open("words.txt") as f:
-                words = f.read().split("\n")
-        else:
-            words = self.get_words(subreddits)
+        files = os.listdir(path)
+        words = self.get_words(files)
+        self.subreddits = self.get_subreddits(files)
+        self.order = []
+        self.sentiments = {
+            "normal": [],
+            "maximum": [],
+            "minimum": [],
+            "all" : []
+        }
+        self.set_sentiments(words)
 
-        sents = {}
-#        if "sentiment.npy" in subreddits:
-#            self.sentiment = np.load("sentiment.npy")
-#            self.sentiment_min = np.load("sentimentMin.npy")
-#            self.sentiment_max = np.load("sentimentMax.npy")
-#            return
+    def set_sentiments(self, words):
+        """Process sentiments in all subreddits, thus create feature matrix
 
-        for subreddit in subreddits:
-            sentiment_normal = []
-            sentiment_minimum = []
-            sentiment_maximum = []
-            sentiment_all = []
+        Arguments:
+            words: list of all words used in the subreddits
+        """
+        for sent in self.subreddits.keys():
+            normal, maximum, minimum, sentiment_all = self.get_sentiment(words, sent)
+            self.sentiments["normal"].append(normal)
+            self.sentiments["maximum"].append(maximum)
+            self.sentiments["minimum"].append(minimum)
+            self.sentiments["all"].append(sentiment_all)
+        self.check_sentiments()
+        self.transform_sentiments()
+
+    def transform_sentiments(self):
+        """ Transform list of list of word sentiments into a numpy array """
+        for view in self.sentiments.keys():
+            self.sentiments[view] = np.array(self.sentiments[view])
+
+    def get_sentiment(self, words, sent):
+        """
+        Create feature vector of a subreddit
+
+        Arguments:
+            words: list of all words used in the subreddits
+            sent: name of the subreddit to process
+        Returns:
+            three lists containing either:
+                - sentiment of each word:
+                  either value in subreddit or 0 if word is not in subreddit
+                - minimum sentiment of each word or 0
+                - maximum sentiment of each word or 0
+                - original sentiment, minimum sentiment, maximum sentiment or 0, 0, 0
+        """
+        sentiment_normal = []
+        sentiment_maximum = []
+        sentiment_minimum = []
+        sentiment_all = []
+        for word in words:
+            if word not in self.subreddits[sent].keys():
+                sentiment_min, sentiment, sentiment_max = 0, 0,0
+            else:
+                sentiment, variance = self.subreddits[sent][word]
+                sentiment_min = round(sentiment - variance, 2)
+                sentiment_max = round(sentiment + variance, 2)
+            sentiment_normal.append(sentiment)
+            sentiment_minimum.append(sentiment_min)
+            sentiment_maximum.append(sentiment_max)
+            sentiment_all.extend([sentiment_min, sentiment, sentiment_max])
+        return sentiment_normal, sentiment_minimum, sentiment_maximum, sentiment_all
+
+    def check_sentiments(self):
+        """ simple check of dimensions of the feature matrices """
+        normal = self.sentiments["normal"]
+        if len(normal) != len(self.sentiments["minimum"]):
+                raise AssertionError("Length of sentiments is not equal")
+        if len(normal) != len(self.sentiments["all"]):
+            raise AssertionError("Length of sentiment vectors is not correct!")
+
+    def get_subreddits(self, files):
+        """
+        Create or load subreddit dictionary
+
+        Arguments:
+            files: list of files that will be used to create subreddit dictionary
+
+        Returns:
+            dictionary of subreddits
+        """
+        if "subreddits.json" in files:
+            with open("subreddits.json") as f:
+                return json.load(f)
+        subreddits = {}
+        for subreddit in files:
             if not subreddit.endswith("tsv"):
+                # file is not a subreddit
                 continue
             try:
                 with open(subreddit) as f:
+                    subreddits[subreddit] = {}
                     tsvreader = csv.reader(f, delimiter="\t")
                     for line in tsvreader:
                         sentiment = float(line[1])
                         standard_variance = float(line[2])
-                        sents[line[0]] = [sentiment, standard_variance]
+                        subreddits[subreddit][line[0]] = [sentiment, standard_variance]
             except Exception as e:
-#               raise TypeError("Cannot read file %s" % subreddit)
                 print(e)
+        with open("subreddits.json", "w") as f:
+            json.dump(subreddits, f)
+        return subreddits
 
-            for word in words:
-                if word not in sents.keys():
-                    sentiment_min, sentiment, sentiment_max = 0, 0,0
-                else:
-                    sentiment, variance = sents[word]
-                    sentiment_min, sentiment_max = round(sentiment - variance, 2), round(sentiment + variance, 2)
-                sentiment_normal.append(sentiment)
-                sentiment_minimum.append(sentiment_min)
-                sentiment_maximum.append(sentiment_max)
-                sentiment_all.extend([sentiment_min, sentiment, sentiment_max])
-               # quick test
-            if len(sentiment_normal) != len(sentiment_minimum):
-                raise AssertionError("Length of sentiments is not equal")
-            if 3*len(sentiment_normal) != len(sentiment_all):
-                print("sentiment_normal %s" % len(sentiment_normal))
-                print("sentiment_all %s" % 3*len(sentiment_all))
-                raise AssertionError("Length of all sentiment vector is not correct!")
-            self.subreddits_sentiments[subreddit] = np.array(sentiment_normal)
-            self.subreddits_sentiments_minimum[subreddit] = np.array(sentiment_minimum)
-            self.subreddits_sentiments_maximum[subreddit] = np.array(sentiment_maximum)
-            self.subreddits_sentiments_all[subreddit] = np.array(sentiment_all)
+    def get_words(self, files):
+        """
+        Create or load list of all words in the subreddits
 
+        Arguments;
+            files: list of files that will be used to create word list
 
-    def get_words(self, subreddits):
+        Returns:
+            list of all words in the subreddits
+        """
+        if "words.txt" in files:
+            with open("words.txt") as f:
+                words = f.read().split("\n")
+        else:
+            print("Need to get all words in subreddits first. This will take up to 2 hours.")
+            words = self.get_words_from_scratch(subreddits)
+        return words
+
+    def get_words_from_scratch(self, files):
+        """ Create word list out of all given subreddits """
         words = set({})
         nlp = spacy.load("en")
-        for subreddit in subreddits:
+        for subreddit in files:
             if not subreddit.endswith("tsv"):
                 continue
             try:
@@ -97,79 +189,75 @@ class SubredditData:
         return words
 
 
-#        np.save("sentiment.npy", self.subreddits_sentiment)
-#        np.save("sentimentMin.npy", self.sentiment_min)
-#        np.save("sentimentMax.npy", self.sentiment_max)
+    def cluster(self, algorithm, args, kwds, name, view):
+        """
+        Cluster the subreddits and save results in a npy-file
 
+        Arguments:
+            algorithm: the algorithm used for clustering (method call!)
+            args: all arguments to be passed to the algorithm method
+            kwds: all  arguments to be passed to the algorithm method
+            name: name of the file that will be used to save the result
+            view: sentiment view (normal, maximum, normal, all)
+        """
+        labels = algorithm(*args, **kwds).fit_predict(self.sentiments[view])
+        np.save(name+"_labels.npy", labels)
 
+    def start_cluster(self, view, times):
+        """
+        Method to start clustering, resutls are saved in a seperate file
 
-def plot_clusters(data, algorithm, args, kwds, name):
-    labels = algorithm(*args, **kwds).fit_predict(data)
-    np.save(name+"_labels.npy", labels)
-    palette = sns.color_palette('deep', np.unique(labels).max() + 1)
-    colors = [palette[x] if x >= 0 else (0.0, 0.0, 0.0) for x in labels]
-#    plt.scatter([i for i in range(len(data))], data, c=colors, **plot_kwds)
-#    frame = plt.gca()
-#    frame.axes.get_xaxis().set_visible(False)
- #   frame.axes.get_yaxis().set_visible(False)
- #   plt.title('Clusters found by {}'.format(str(algorithm.__name__)), fontsize=24)
+        Arguments:
+            view: whether to use original sentiments (normal), negative values (minimum),
+                maximum values (maximum) or all three (all)
+            times:
+                maximum number of clusters to be used for KMeans, spectral and
+                agglomerative clustering
+        """
+        for number in range(3, times):
+            self.cluster(
+                cluster.MiniBatchKMeans,
+                (),
+                {'n_clusters':number},
+                "mini_batch_kmeans"+str(number)+view,
+                view
+            )
+            self.cluster(
+                cluster.SpectralClustering,
+                (),
+                {'n_clusters':number},
+                "spectral"+str(number)+view,
+                view
+            )
+            self.cluster(
+                cluster.AgglomerativeClustering,
+                (),
+                {'n_clusters':number, 'linkage':'ward'},
+                "aggl" + str(number) + view,
+                view
+            )
+        self.cluster(
+            cluster.MeanShift,
+            (0.175,),
+            {'cluster_all':False},
+            "mean_shift" + view,
+            view
+        )
+        self.cluster(
+            hdbscan.HDBSCAN,
+            (),
+            {'min_cluster_size':15},
+            "HDBSCAN" + view,
+            view
+        )
+
 
 
 if __name__ == '__main__':
-    subreddits = SubredditData(".")
-    plot_kwds = {'alpha' : 0.25, 's' : 80, 'linewidths':0}
-#    arrays = np.array([x for x in subreddits.subreddits_sentiments.values()]).reshape(-1, 1)
-    #arrays = np.array([x for x in subreddits.subreddits_sentiments.values()])
-    keys = []
-    value_normal = []
-    value_minimum = []
-    value_maximum = []
-    value_all = []
-    for key in subreddits.subreddits_sentiments.keys():
-        keys.append(key)
-        value_normal.append(subreddits.subreddits_sentiments[key])
-        value_minimum.append(subreddits.subreddits_sentiments_minimum[key])
-        value_maximum.append(subreddits.subreddits_sentiments_maximum[key])
-        value_all.append(subreddits.subreddits_sentiments_all[key])
-    arrays = np.array(value_normal)
-    with open("subreddits.txt", "w") as f:
-        f.write("\n".join(keys))
-    np.save("sentiments_normal.npy", arrays)
-    arrays = np.array(value_normal)
-    for number in range(3, 100):
-        plot_clusters(arrays, cluster.MiniBatchKMeans, (), {'n_clusters':number}, "mini_batch_kmeans"+str(number))
-        plot_clusters(arrays, cluster.SpectralClustering, (), {'n_clusters':number}, "spectral"+str(number))
-        plot_clusters(arrays, cluster.AgglomerativeClustering, (), {'n_clusters':number, 'linkage':'ward'}, "aggl"+str(number))
-    plot_clusters(arrays, cluster.MeanShift, (0.175,), {'cluster_all':False}, "mean_shift")
-    plot_clusters(arrays, hdbscan.HDBSCAN, (), {'min_cluster_size':15}, "HDBSCAN")
-
-    arrays = np.array(value_minimum)
-    np.save("sentiments_min.npy", arrays)
-    for number in range(3, 100):
-        plot_clusters(arrays, cluster.MiniBatchKMeans, (), {'n_clusters':number}, "mini_batch_kmeans_min"+str(number))
-        plot_clusters(arrays, cluster.SpectralClustering, (), {'n_clusters':number}, "spectral_min"+str(number))
-        plot_clusters(arrays, cluster.AgglomerativeClustering, (), {'n_clusters':number, 'linkage':'ward'}, "aggl_min"+str(number))
-    plot_clusters(arrays, cluster.MeanShift, (0.175,), {'cluster_all':False}, "mean_shift_min")
-    plot_clusters(arrays, cluster.DBSCAN, (), {'eps':0.025}, "DBSCAN_min")
-    plot_clusters(arrays, hdbscan.HDBSCAN, (), {'min_cluster_size':15}, "HDBSCAN_min")
-
-    arrays = np.array(value_maximum)
-    np.save("sentiments_max.npy", arrays)
-    for number in range(3, 100):
-        plot_clusters(arrays, cluster.MiniBatchKMeans, (), {'n_clusters':number}, "mini_batch_kmeans_min"+str(number))
-        plot_clusters(arrays, cluster.SpectralClustering, (), {'n_clusters':number}, "spectral_min"+str(number))
-        plot_clusters(arrays, cluster.AgglomerativeClustering, (), {'n_clusters':number, 'linkage':'ward'}, "aggl_min"+str(number))
-    plot_clusters(arrays, cluster.MeanShift, (0.175,), {'cluster_all':False}, "mean_shift_max")
-    plot_clusters(arrays, cluster.DBSCAN, (), {'eps':0.025}, "DBSCAN_max")
-    plot_clusters(arrays, hdbscan.HDBSCAN, (), {'min_cluster_size':15}, "HDBSCAN_max")
-
-
-    arrays = np.arrays(value_all)
-    np.save("sentiments_all.npy", arrays)
-    for number in range(3, 100):
-        plot_clusters(arrays, cluster.MiniBatchKMeans, (), {'n_clusters':number}, "mini_batch_kmeans_min"+str(number))
-        plot_clusters(arrays, cluster.SpectralClustering, (), {'n_clusters':number}, "spectral_min"+str(number))
-        plot_clusters(arrays, cluster.AgglomerativeClustering, (), {'n_clusters':number, 'linkage':'ward'}, "aggl_min"+str(number))
-    plot_clusters(arrays, cluster.MeanShift, (0.175,), {'cluster_all':False}, "mean_shift_all")
-    plot_clusters(arrays, cluster.DBSCAN, (), {'eps':0.025}, "DBSCAN_all")
-    plot_clusters(arrays, hdbscan.HDBSCAN, (), {'min_cluster_size':15}, "HDBSCAN_all")
+    path = "/home/students/hubert/socialsent/socialsent/socialsent/data/lexicons/subreddits"
+    subreddits = SubredditData(path)
+    print("Subreddit Class works!")
+    subreddits.start_cluster("normal", 300)
+    subreddits.start_cluster("minimum", 300)
+    subreddits.start_cluster("maximum", 300)
+    subreddits.start_cluster("all", 300)
