f014bf68484da1772e3013b7b1298dbc10e83e3a
hubert <hubert@cl.uni-heidelberg.de>
2017-08-06 12:37:48 +0200

    complete process

--- evaluate.sh
+++ evaluate.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# cluster data first
+python3 cluster.py
+# evaluate clusters
 python3 evaluate_all.py normal > comparision_labels_normal.txt
 python3 evaluate_all.py minimum > comparision_labels_minimum.txt
 python3 evaluate_all.py maximum > comparision_labels_maximum.txt
