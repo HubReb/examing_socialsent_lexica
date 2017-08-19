5a681294db1bbed3e5679de8bcb58d66aa710ed8
hubert <hubert@cl.uni-heidelberg.de>
2017-08-19 16:18:48 +0200

    add abstract class, add doc strings

--- subreddit_data.py
+++ subreddit_data.py
@@ -15,16 +15,14 @@ subreddits. For now, all clustering algorithms are applied.
 '''
 
 import os
-import numpy as np
 import pandas as pd
 
-from subreddit_helpers import get_words, get_subreddits
+from helpers import get_subreddits
 from constants import PATH
+from data import Data
 
-#PATH = "/home/students/hubert/socialsent/socialsent/socialsent/data/lexicons/subreddits"
-
-class SubredditData:
-    """
+class SubredditData(Data):
+    '''
     An object containing all raw and processed data in every given subreddit.
 
     Attributes:
@@ -35,60 +33,56 @@ class SubredditData:
             of all subreddits
         order:
             list representing the order of the subreddits in the sentiment dictionary
-    """
+    '''
 
     def __init__(self, path):
-        """
+        '''
         Initialize an object containing all raw and processed data
 
         Arguments:
             path:   Path to the folder containing the subreddits
-        """
-
-        files = os.listdir(path)
-        words = get_words(files)
-        self.subreddits = get_subreddits(files)
+        '''
+        super().__init__(path)
+        self.subreddits = get_subreddits(self.files)
         self.order = []
         self.sentiments = {
-            "normal": [],
-            "maximum": [],
-            "minimum": [],
-            "all" : []
+            'normal': [],
+            'maximum': [],
+            'minimum': [],
+            'all' : []
         }
-        self.set_sentiments(words)
+        self.set_sentiments()
         self.save_order()
 
-    def set_sentiments(self, words):
-        """Process sentiments in all subreddits, thus create feature matrix
+    def set_sentiments(self):
+        '''Process sentiments in all subreddits, thus create feature matrix
 
         Arguments:
             words: list of all words used in the subreddits
-        """
+        '''
         for sent in self.subreddits.keys():
             self.order.append(sent)
-            normal, maximum, minimum, sentiment_all = self.get_sentiment(words, sent)
-            self.sentiments["normal"].append(normal)
-            self.sentiments["maximum"].append(maximum)
-            self.sentiments["minimum"].append(minimum)
-            self.sentiments["all"].append(sentiment_all)
+            normal, maximum, minimum, sentiment_all = self.get_sentiment(self.words, sent)
+            self.sentiments['normal'].append(normal)
+            self.sentiments['maximum'].append(maximum)
+            self.sentiments['minimum'].append(minimum)
+            self.sentiments['all'].append(sentiment_all)
         self.check_sentiments()
         self.transform_sentiments()
+        self.create_data_frame()
+
+    def create_data_frame(self):
         for view, sentiments in self.sentiments.items():
             if view == 'all':
                 extended_words = []
-                for word in words:
-                    extended_words.extend([word + "min", word, word + "max"])
+                for word in self.words:
+                    extended_words.extend([word + 'min', word, word + 'max'])
                 self.sentiments[view] = pd.DataFrame(sentiments, columns=extended_words)
             else:
-                self.sentiments[view] = pd.DataFrame(sentiments, columns=words)
-
-    def transform_sentiments(self):
-        """ Transform list of list of word sentiments into a numpy array """
-        for view, values in self.sentiments.items():
-            self.sentiments[view] = np.array(values)
+                self.sentiments[view] = pd.DataFrame(sentiments, columns=self.words)
 
     def get_sentiment(self, words, sent):
-        """
+        '''
         Create feature vector of a subreddit
 
         Arguments:
@@ -100,14 +94,15 @@ class SubredditData:
                   either value in subreddit or 0 if word is not in subreddit
                 - minimum sentiment of each word or 0
                 - maximum sentiment of each word or 0
-                - original sentiment, minimum sentiment, maximum sentiment or 0, 0, 0
-        """
+                - original sentiment, minimum sentiment, maximum sentiment
+        '''
         sentiment_normal = []
         sentiment_maximum = []
         sentiment_minimum = []
         sentiment_all = []
+        words_with_sentiment = self.subreddits[sent].keys()
         for word in words:
-            if word not in self.subreddits[sent].keys():
+            if word not in words_with_sentiment:
                 sentiment_min, sentiment, sentiment_max = 0, 0, 0
             else:
                 sentiment, variance = self.subreddits[sent][word]
@@ -120,18 +115,13 @@ class SubredditData:
         return sentiment_normal, sentiment_minimum, sentiment_maximum, sentiment_all
 
     def check_sentiments(self):
-        """ simple check of dimensions of the feature matrices """
-        normal = self.sentiments["normal"]
-        if len(normal) != len(self.sentiments["minimum"]):
-            raise AssertionError("Length of sentiments is not equal")
-        if len(normal) != len(self.sentiments["all"]):
-            raise AssertionError("Length of sentiment vectors is not correct!")
-
-    def save_order(self):
-        """ save order of reddits in featurevectors """
-        with open("order_subreddits.txt", "w") as order_in_file:
-            order_in_file.write("\n".join(self.order))
+        ''' simple check of dimensions of the feature matrices '''
+        normal = self.sentiments['normal']
+        if len(normal) != len(self.sentiments['minimum']):
+            raise AssertionError('Length of sentiments is not equal')
+        if len(normal) != len(self.sentiments['all']):
+            raise AssertionError('Length of sentiment vectors is not correct!')
 
 if __name__ == '__main__':
     subreddits = SubredditData(PATH)
-    print("Subreddit Class works!")
+    print('Subreddit Class works!')
