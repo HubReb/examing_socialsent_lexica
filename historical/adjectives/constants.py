964d2e21ad8f8022ff9355a9302430076f9f86b3
hubert <hubert@cl.uni-heidelberg.de>
2017-08-22 19:49:06 +0200

    delete double files

--- historical/adjectives/constants.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-''' Constants used in various scipts '''
-
-PATH = "/home/students/hubert/socialsent/socialsent/socialsent/data/lexicons/subreddits/"
-PATH_HISTORICAL_ADJECTIVES = "/home/students/hubert/socialsent/socialsent/socialsent/data/lexicons/subreddits/historical/adjectives"
-ACCEPTABLE_OPTIONS = ["normal", "minimum", "maximum", "all"]
