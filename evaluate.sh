de37aec364f78bef19afd6e82c987e15c086393c
hubert <hubert@cl.uni-heidelberg.de>
2017-08-25 18:43:09 +0200

    fix script

--- evaluate.sh
+++ evaluate.sh
@@ -14,7 +14,7 @@ for i in {2..200}; do
     python3 evaluate_subreddits.py normal -r subreddits_results -c $i > compas/comparision_labels_normal$i
     python3 evaluate_subreddits.py minimum -r subreddits_results -c $i > compas/comparision_labels_minimum$i
     python3 evaluate_subreddits.py maximum -r subreddits_results -c $i > compas/comparision_labelsi_maximum$i
-    python3 evaluate_subreddits.py all -r subreddits_results -c $i> comparision_labels_all$i
+    python3 evaluate_subreddits.py all -r subreddits_results -c $i> compas/comparision_labels_all$i
 done
 for i in {2..8}; do
     python3 evaluate_historical.py frequencies -r frequencies_results -c $i > compas/comparision_labels_adjectives$i
