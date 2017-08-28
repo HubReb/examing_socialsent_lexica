883b7a2a1c168c76242895b790fead67b5303fd6
hubert <hubert@cl.uni-heidelberg.de>
2017-08-28 20:49:08 +0200

    fix min cluter size

--- examinlexica/outlier.py
+++ examinlexica/outlier.py
@@ -1,11 +1,13 @@
 import hdbscan
 import seaborn as sns
 import numpy as np
+import matplotlib.pyplot as plt
 
 from constants import PATH_HISTORICAL_ADJECTIVES
 from original.historical_data import HistoricalData
 
 data = HistoricalData(PATH_HISTORICAL_ADJECTIVES)
-clusterer = hdbscan.HDBSCAN(min_cluster_size=15).fit(data.sentiments)
+clusterer = hdbscan.HDBSCAN(min_cluster_size=2).fit(data.sentiments)
 print(clusterer.outlier_scores_)
 sns.distplot(clusterer.outlier_scores_[np.isfinite(clusterer.outlier_scores_)], rug=True)
+plt.show()
