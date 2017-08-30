3edc5b5bf0864207a7d52a5529375360d96ac836
hubert <hubert@cl.uni-heidelberg.de>
2017-08-30 17:20:54 +0200

    fix evalutation formular

--- examinlexica/evaluate.sh
+++ examinlexica/evaluate.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-echo "Evaluating everything. This will take up to 30 hours!"
+echo "Evaluating everything. This will take up to 40 hours!"
 # cluster data first
-python3 cluster.py subreddits -r subreddits -c 200 -m normal
 mkdir compas
-for i in {2..200}; do
-    python3 evaluate/evaluate_subreddits.py normal -r subreddits_results -c $i > compas/comparision_labels_normal$i
+for i in {2..125}; do
+    python3 cluster.py subreddits -r subreddits_cosine_infinity -c $i -m normal
+    python3 evaluate/evaluate_subreddits.py normal -r subreddits_cosine_infinity_results -c $i > compas/comparision_labels_normal$i
 done
 cp ../order.txt compas/order_normal.txt
-python3 cluster.py subreddits -r subreddits -c 200 -m minimum
-for i in {2..200}; do
-    python3 evaluate/evaluate_subreddits.py minimum -r subreddits_results -c $i > compas/comparision_labels_minimum$i
+for i in {2..125}; do
+    python3 cluster.py subreddits -r subreddits -c $i -m minimum
+    python3 evaluate/evaluate_subreddits.py minimum -r subreddits_cosine_infinity_results -c $i > compas/comparision_labels_minimum$i
 done
 cp ../order.txt compas/order_minimum.txt
-python3 cluster.py subreddits -r subreddits -c 200 -m maximum
-for i in {2..200}; do
-    python3 evaluate/evaluate_subreddits.py maximum -r subreddits_results -c $i > compas/comparision_labels_maximum$i
+for i in {2..125}; do
+    python3 cluster.py subreddits -r subreddits -c $i -m maximum
+    python3 evaluate/evaluate_subreddits.py maximum -r subreddits_cosine_infinity_results -c $i > compas/comparision_labels_maximum$i
 done
 cp ../order.txt compas/order_maximum.txt
-python3 cluster.py subreddits -r subreddits -c 200 -m all
-for i in {2..200}; do
-    python3 evaluate/evaluate_subreddits.py all -r subreddits_results -c $i > compas/comparision_labels_all$i
+for i in {2..125}; do
+    python3 cluster.py subreddits -r subreddits -c $i -m all
+    python3 evaluate/evaluate_subreddits.py all -r subreddits_cosine_infinity_results -c $i > compas/comparision_labels_all$i
 done
 cp ../order.txt compas/order_all.txt
 
-python3 cluster.py frequencies -r frequencies -c 8
 for i in {2..8}; do
-    python3 evaluate/evaluate_historical.py frequencies -r frequencies_results -c $i > compas/comparision_labels_adjectives$i
+    python3 cluster.py frequencies -r frequencies -c $i
+    python3 evaluate/evaluate_historical.py frequencies -r frequencies_results -c $i > compas/comparision_labels_frequencies$i
 done
-python3 cluster.py adjectives -r adjectives -c 8
 # evaluate clusters
 for i in {2..8}; do
+    python3 cluster.py adjectives -r adjectives -c $i
     python3 evaluate/evaluate_historical.py adjectives -r adjectives_results -c $i > compas/comparision_labels_adjectives$i
 done
